e9b856e69607b61917736fb996702163a36bdf85
Santhosh2141 <snthshsrinivas@gmail.com>
2025-05-29 20:59:19 +0530

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Santhosh Srinivas on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
